2dad19d87cb3c6838376a53fc49c8d122ee5b4f7
Paulo Mariano <paulomariano2501@gmail.com>
2025-03-09 23:28:56 -0300

    feat: change name

--- src/components/PDFReader.tsx
+++ src/components/PDFReader.tsx
@@ -12,7 +12,7 @@ declare global {
 }
 
 const PDFReader = () => {
-	const [file, setFile] = useState<string>("");
+	const [textContent, setTextContent] = useState<string>("");
 
 	useEffect(() => {
 		const script = document.createElement("script");
@@ -49,7 +49,7 @@ const PDFReader = () => {
 								if (pageNum < totalPages) {
 									extractTextFromPage(pageNum + 1);
 								} else {
-									setFile(textContent);
+									setTextContent(textContent);
 								}
 							});
 						});
@@ -67,19 +67,20 @@ const PDFReader = () => {
 
 	return (
 		<main className="flex flex-col md:flex-row flex-grow">
-			<div className="flex-1 flex items-start justify-center p-4">
+			<div className="flex-1 flex items-start justify-center p-4 animate-fade-slide-up">
 				<div className="w-full flex flex-col border-2 shadow-lg border-gray-200 rounded-lg p-4">
 					<h2 className="text-xl font-bold mb-4">Upload Your Resume</h2>
 					<InputFile handleFileChange={handleFileChange} />
 					<p className="text-[#5664f5] mt-2">Attach your resume in PDF format to get started.</p>
 
-					<Button variant="default" disabled className="mt-4 bg-[#5664f5] hover:bg-[#4b55e8] text-white font-bold">
+					<Button variant="default" disabled={!textContent} className="mt-4 bg-[#5664f5] hover:bg-[#4b55e8] text-white font-bold">
 						Generate Cover Letter
 					</Button>
 				</div>
 			</div>
-			<div className="flex-1 flex items-start h-full bg-white p-4">
-				<div className="w-full flex flex-col gap-4 border-2 shadow-lg h-full border-gray-200 rounded-lg p-4">
+
+			<div className="flex-1 flex items-start h-full p-4 animate-fade-slide-up">
+				<div className="w-full flex flex-col gap-4 border-2 shadow-sm h-full border-gray-200 rounded-lg p-4">
 					<h2 className="text-xl font-bold mb-4">Generated Cover Letter</h2>
 
 					<TextAreaReadOnly />
