b3a0f6db663aee12324d10696baf2cbf62f80f5b
Paulo Mariano <paulomariano2501@gmail.com>
2025-03-10 22:04:09 -0300

    feat: add redirect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import { ScrollText } from "lucide-react";
 const Header = () => {
 	return (
 		<header className="flex justify-between items-center w-full p-4 shadow-md animate-add-shadow">
-			<span className="flex items-center">
+			<a href="/" className="flex items-center" rel="noreferrer">
 				<ScrollText color="#5664f5" size={45} />
 				<h1 className="text-xl sm:text-2xl font-bold ml-2">Cover Letter Generator</h1>
-			</span>
+			</a>
 		</header>
 	);
 };
