a44fe2e2971dfbefea2deda1ec28f81080c010a8
Paulo Mariano <paulomariano2501@gmail.com>
2025-03-10 23:51:01 -0300

    fix: maybe fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
 	return (
 		<header className="flex justify-between items-center w-full p-4 shadow-md animate-add-shadow">
 			<a href="/" className="flex items-center" rel="noreferrer">
-				<img src="src\assets\logo.png" alt="Logo" className="w-10 h-10" />
+				<img src="src/assets/logo.png" alt="Logo" className="w-9 h-9" />
 				<h1 className="text-xl sm:text-2xl font-bold ml-2 bg-gradient-to-r from-[#2500f5] to-[#5664f5] text-transparent bg-clip-text">Cover Letter Generator</h1>
 			</a>
 		</header>
