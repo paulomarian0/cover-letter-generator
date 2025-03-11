0f32db667b1623dd49f9bb163621d3230c7e9aa7
Paulo Mariano <paulomariano2501@gmail.com>
2025-03-10 21:16:25 -0300

    fix

--- src/components/ui/text-area-read-only.tsx
+++ src/components/ui/text-area-read-only.tsx
@@ -1,7 +1,7 @@
 export default function TextAreaReadOnly({ value }: { value: string }) {
 	return (
 		<div className="flex flex-1 min-h-0">
-			<div className="flex-1 h-[430px] overflow-auto p-2 border rounded-md bg-muted whitespace-pre-wrap">{value}</div>
+			<div className="flex-1 h-[360px] overflow-auto p-2 border rounded-md bg-muted whitespace-pre-wrap">{value}</div>
 		</div>
 	);
 }
